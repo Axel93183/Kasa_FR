faa0be0eb64df55305e1beab9d8a29f248ce38e7
Axel GMX <agmx183@gmail.com>
2024-07-05 12:34:51 +0200

    Structure du projet : Routage dans App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import "./App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
-import MyRouter from "./routes/MyRouter";
+
+import About from "./pages/About/About.js";
+import Error404 from "./pages/Error-404/Error-404.js";
+import Home from "./pages/Home/Home.js";
+import Housing from "./pages/Housing/Housing.js";
+
+import "./App.scss";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <MyRouter />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/a-propos" element={<About />} />
+          <Route path="/fiche-logement/:id" element={<Housing />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
         <Footer />
       </div>
     </Router>
